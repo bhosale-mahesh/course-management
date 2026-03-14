9ccf15c9596f11dc027f2d8aaa3a784bab2e7810
Mahesh Bhosale <233565011+bhosale-mahesh@users.noreply.github.com>
2026-03-14 22:12:12 +0530

    Add find by name api for instructor and add transactional

--- src/main/java/com/mb/repository/InstructorRepository.java
+++ src/main/java/com/mb/repository/InstructorRepository.java
@@ -1,9 +1,13 @@
 package com.mb.repository;
 
 import com.mb.model.Instructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InstructorRepository extends JpaRepository<Instructor, Long> {
 
     boolean existsByEmail(String email);
+
+    Page<Instructor> findByNameContainingIgnoreCase(String name, Pageable pageable);
 }
