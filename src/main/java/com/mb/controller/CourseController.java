f3c543fe4840518e2a77f0d77f5a5c473cddab5e
Mahesh Bhosale <233565011+bhosale-mahesh@users.noreply.github.com>
2026-03-15 14:44:58 +0530

    Add create course API

--- src/main/java/com/mb/controller/CourseController.java
+++ src/main/java/com/mb/controller/CourseController.java
@@ -1,15 +1,21 @@
 package com.mb.controller;
 
+import com.mb.dto.request.CourseRequest;
 import com.mb.dto.response.CourseResponse;
 import com.mb.dto.response.PaginatedResponse;
 import com.mb.service.CourseService;
 import com.mb.util.PaginationUtil;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,5 +31,11 @@ public class CourseController {
         return PaginationUtil.buildPaginatedResponse(
                 courseService.getAllCourses(PageRequest.of(page, size, Sort.by("id")))
         );
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CourseResponse createCourse(@Valid @RequestBody CourseRequest courseRequest) {
+        return courseService.createCourse(courseRequest);
     }
 }
--- src/main/java/com/mb/service/CourseService.java
+++ src/main/java/com/mb/service/CourseService.java
@@ -1,8 +1,11 @@
 package com.mb.service;
 
+import com.mb.dto.request.CourseRequest;
 import com.mb.dto.response.CourseResponse;
 import com.mb.model.Course;
+import com.mb.model.Instructor;
 import com.mb.repository.CourseRepository;
+import com.mb.repository.InstructorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class CourseService {
 
     private final CourseRepository courseRepository;
+    private final InstructorRepository instructorRepository;
 
     private CourseResponse toCourseResponse(Course course) {
         return CourseResponse.builder()
@@ -28,5 +32,20 @@ public class CourseService {
     @Transactional(readOnly = true)
     public Page<CourseResponse> getAllCourses(Pageable pageable) {
         return courseRepository.findAll(pageable).map(this::toCourseResponse);
+    }
+
+    @Transactional
+    public CourseResponse createCourse(CourseRequest courseRequest) {
+        Instructor instructor = instructorRepository.findById(courseRequest.instructorId()).orElseThrow(
+                () -> new RuntimeException("Instructor with id " + courseRequest.instructorId() + " not found")
+        );
+        Course course = Course.builder()
+                .title(courseRequest.title())
+                .description(courseRequest.description())
+                .instructor(instructor)
+                .price(courseRequest.price())
+                .build();
+        Course savedCourse = courseRepository.save(course);
+        return toCourseResponse(savedCourse);
     }
 }
