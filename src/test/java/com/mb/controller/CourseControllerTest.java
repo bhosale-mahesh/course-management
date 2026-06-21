4d30dc960038c67288b062555dec1bd0e87367f2
Mahesh Bhosale <233565011+bhosale-mahesh@users.noreply.github.com>
2026-06-21 20:54:18 +0530

    Add test for getAllCourses

--- src/test/java/com/mb/controller/CourseControllerTest.java
+++ src/test/java/com/mb/controller/CourseControllerTest.java
@@ -1,24 +1,72 @@
 package com.mb.controller;
 
+import com.mb.dto.response.CourseResponse;
 import com.mb.service.CourseService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @WebMvcTest(CourseController.class)
 class CourseControllerTest {
 
+    @Autowired
+    MockMvc mockMvc;
+
     @MockitoBean
     CourseService courseService;
 
-    @BeforeEach
-    void setUp() {
+    private static final String COURSE_URL = "/v1/api/courses";
+
+    private List<CourseResponse> getCoursesList() {
+        return List.of(
+                CourseResponse.builder()
+                        .id(1L)
+                        .title("Advanced Potion Making")
+                        .description("Master the art of brewing magical potions")
+                        .price(BigDecimal.valueOf(120L))
+                        .build()
+        );
+    }
+
+    private Page<CourseResponse> getCourses() {
+        List<CourseResponse> courses = getCoursesList();
+        return new PageImpl<>(
+                courses,
+                PageRequest.of(0, 10),
+                courses.size()
+        );
     }
 
     @Test
-    void getAllCourses() {
+    void getAllCourses() throws Exception {
+        when(courseService.getAllCourses(any(Pageable.class)))
+                .thenReturn(getCourses());
+
+        mockMvc.perform(MockMvcRequestBuilders.get(COURSE_URL)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data", hasSize(1)))
+                .andExpect(jsonPath("$.data[0].title").value("Advanced Potion Making"));
     }
 
     @Test
